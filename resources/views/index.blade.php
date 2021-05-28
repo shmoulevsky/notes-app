c0a7ec80c81d366fefbecc86f3641333338dad3b
Eugeny Shmoulevsky <shmoulevskye@gmail.com>
2021-05-28 17:52:36 +0700

    fix text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('/layouts/master') 
 @section('title', $note->name ?? 'Главная') 
 @section('content')
-<div contenteditable="true" class="note-wrap" style="width: 100%; min-height: 300px; margin-bottom: 30px;" class="col-12">{{$note->text ?? 'Добро пожаловать!'}}</div>
+<div contenteditable="true" class="note-wrap" style="width: 100%; min-height: 300px; margin-bottom: 30px;" class="col-12">{!! $note->text ?? 'Добро пожаловать!' !!}</div>
 <button type="button" class="btn btn-primary update-note-btn float-right">Сохранить</button> 
 
 <div id="add-note-modal" class="modal fade" role="dialog" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
